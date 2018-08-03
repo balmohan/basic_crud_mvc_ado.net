1b3107ee5c0ce2a09084f7633ae0686a03b1e168
balmohan <balmohan.job@gmail.com>
2018-08-03 12:09:22 +0530

    initial commit

---
+++ dbconnection/DatabaseConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace StudentDbManagement.Database
+{
+    public class DatabaseConnection
+    {
+        private SqlConnection conn=null;
+        //private DatabaseConnection() { }
+        public  SqlConnection getconnection
+        {
+            get
+            {
+                if (conn != null)
+                {
+                    return conn;
+                }
+                else
+                {
+                    try
+                    {
+                        conn = new SqlConnection(getConnectionString());
+                        return conn;
+                    }
+                    catch(SqlException)
+                    {
+                        throw;
+                    }
+                }
+                
+            }
+        }
+
+        public  string getConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+        }
+    }
+}
