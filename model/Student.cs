9f8a36e92cb8416519e2a66d4dbbc846ad79701a
balmohan <balmohan.job@gmail.com>
2018-08-03 12:04:55 +0530

    initial commit

---
+++ model/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentDbManagement.Models
+{
+    public class Student
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="First Name")]
+        public string firstName { get; set; }
+        [Required]
+        [Display(Name ="Last Name")]
+        public string lastName { get; set; }
+        [Required]
+        [Display(Name ="Address")]
+        public string address { get; set; }
+        [Display(Name ="Birth Date")]
+        [DataType(DataType.Date)]
+        public DateTime dateOfBirth { get; set; }
+    }
+}
